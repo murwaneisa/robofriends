85d7cc9b71382e266867d99485f841f240fb358d
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2020-05-15 18:56:00 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import 'tachyons';
+// import Card from'./Card';
+import App from'./App';
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	
+  <App/>,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
